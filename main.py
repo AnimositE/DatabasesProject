8d6404bb4b12f8b72d14622603a6c3ec85abb769
Mason Schneider <schneimd@rose-hulman.edu>
2014-01-07 15:31:18 -0500

    Testing postgres connection

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import os
 from flask import Flask
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+	cursor = conn.cursor()
+	#cursor.execute("SELECT * FROM Users")
+	#users = cursor.fetchall()
+    return 'Connected!'
