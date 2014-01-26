224580dd3cb84b284610e327c030b5b821aeda6c
Mason Schneider <schneimd@rose-hulman.edu>
2014-01-26 16:27:04 -0500

    Added python to render basic templates

--- main.py
+++ main.py
@@ -23,7 +23,19 @@ def index():
     #cursor = conn.cursor()
     #cursor.execute("SELECT * FROM Diver")
     #users = cursor.fetchall()
+    return render_template('home.html')
+
+@app.route('/divers/')
+def divers():
     return render_template('divers.html')
+
+@app.route('/meets/')
+def meets():
+    return "no html yet"
+
+@app.route('/dd/')
+def ddLookup():
+    return "no html yet"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
