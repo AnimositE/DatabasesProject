e2db211dfec09d464de9948c43cd22eca2c3fa0a
Mason Schneider <schneimd@rose-hulman.edu>
2014-02-04 14:03:52 -0500

    Testing heroku version

--- database.py
+++ database.py
@@ -20,7 +20,7 @@ class Database:
     # DIVERS ------------------------------------------------------------------------
 
     def getDivers(self):
-        self.cursor.execute("SELECT Divers.id, fName, lName, name FROM Divers, Profiles, Schools WHERE Divers.id = Profiles.diverID AND Profiles.schoolID = Schools.id;")
+        self.cursor.execute("SELECT Divers.id, fName, lName, name FROM Divers, Profiles, Schools WHERE Divers.id = Profiles.diverID AND Profiles.schoolID = Schools.id AND fName NOT NULL AND lName NOT NULL AND schoolID NOT NULL;")
         return self.cursor.fetchall()
 
     def getDiverProfile(self, diverid):
