614060aa6306490abd5060e6ca1954b143747391
Mason Schneider <schneimd@rose-hulman.edu>
2014-02-05 14:42:29 -0500

    Completed meet viewing

--- DiveBook/meets.py
+++ DiveBook/meets.py
@@ -16,9 +16,9 @@ def meet(id):
     meet = [1,'Rose-Hulman Dive Meet', '5500 Wabash Ave','Terre Haute','IN','47803','2014-02-29']
     count = 13
     divesheets = [[1,'Awesome Divesheet'],[2,'Easy Divesheet']]
-    meet,count = db.getMeet(id)
+    #meet,count = db.getMeet(id)
     if 'id' in session:
         divesheets = [[1,'Awesome Divesheet'],[2,'Easy Divesheet']]
-        divesheets = db.getNonRegisteredDiveSheets(session['id'])
+        #divesheets = db.getNonRegisteredDiveSheets(session['id'])
     return render_template('meet.html',meet=meet,count=count,divesheets=divesheets)
 
