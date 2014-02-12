7cdfb14ef72c32262f02c1417d49e569a9ac12e4
Mason Schneider <schneimd@rose-hulman.edu>
2014-02-11 23:14:38 -0500

    Started adding dives

---
+++ dives.sql
@@ -0,0 +1,38 @@
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Dive','101',1,'Forward','B',1.3);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Dive','101',1,'Forward','C',1.2);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Dive','101',3,'Forward','A',1.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Dive','101',3,'Forward','B',1.5);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Dive','101',3,'Forward','C',1.4);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Somersault','102',1,'Forward','A',1.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Somersault','102',1,'Forward','B',1.5);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Somersault','102',1,'Forward','C',1.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Somersault','102',3,'Forward','A',1.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Somersault','102',3,'Forward','B',1.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward Somersault','102',3,'Forward','C',1.5);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 1 1/2 Somersault','103',1,'Forward','A',2.0);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 1 1/2 Somersault','103',1,'Forward','B',1.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 1 1/2 Somersault','103',1,'Forward','C',1.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 1 1/2 Somersault','103',3,'Forward','A',1.9);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 1 1/2 Somersault','103',3,'Forward','B',1.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 1 1/2 Somersault','103',3,'Forward','C',1.5);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 Somersaults','104',1,'Forward','A',2.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 Somersaults','104',1,'Forward','B',2.3);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 Somersaults','104',1,'Forward','C',2.2);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 Somersaults','104',3,'Forward','A',2.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 Somersaults','104',3,'Forward','B',2.1);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 Somersaults','104',3,'Forward','C',2.0);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 1/2 Somersaults','105',1,'Forward','B',2.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 1/2 Somersaults','105',1,'Forward','C',2.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 1/2 Somersaults','105',3,'Forward','A',2.8);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 1/2 Somersaults','105',3,'Forward','B',2.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 1/2 Somersaults','105',3,'Forward','C',2.2);
+
+-- All of these are entered
+
+
+
+
