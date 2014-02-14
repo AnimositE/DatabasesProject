b853a51caed6d469f2e75d70241392a8ce40ab86
Mason Schneider <schneimd@rose-hulman.edu>
2014-02-14 16:03:24 -0500

    Added profile creation on register

--- DiveBook/meets.py
+++ DiveBook/meets.py
@@ -7,8 +7,8 @@ from DiveBook import app
 def meets():
     upcoming = [[1,'Rose-Hulman','IN','2014-02-19'],]
     past = [[2,'ISU','IN','2014-01-19'],]
-    upcoming = db.getUpcomingMeets()
-    past = db.getPastMeets()
+    #upcoming = db.getUpcomingMeets()
+    #past = db.getPastMeets()
     return render_template('meets.html',upcoming=upcoming,past=past)
 
 @app.route('/meets/<int:id>')
@@ -16,9 +16,9 @@ def meet(id):
     meet = [1,'Rose-Hulman Dive Meet', '5500 Wabash Ave','Terre Haute','IN','47803','2014-02-29']
     count = 13
     divesheets = [[1,'Awesome Divesheet'],[2,'Easy Divesheet']]
-    meet,count = db.getMeet(id)
+    #meet,count = db.getMeet(id)
     if 'id' in session:
         divesheets = [[1,'Awesome Divesheet'],[2,'Easy Divesheet']]
-        divesheets = db.getNonRegisteredDiveSheets(session['id'])
+        #divesheets = db.getNonRegisteredDiveSheets(session['id'])
     return render_template('meet.html',meet=meet,count=count,divesheets=divesheets)
 
