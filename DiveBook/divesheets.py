6a530bc2dce7b587f9a3e68ee4c2f2a410dc5284
taintedcheese <heinmr@rose-hulman.edu>
2014-02-16 16:52:58 -0500

    Errors

--- DiveBook/divesheets.py
+++ DiveBook/divesheets.py
@@ -8,7 +8,7 @@ def divesheets():
 	if 'id' not in session:
 		return redirect(url_for('index'))
 	sheets = [[1,'My Super Awesome Dive Sheet','Rose-Hulman Dive Meet','10.0'],[2,'My Other Dive Sheet','Rose-Hulman Dive Meet','10.0']]
-	sheets = db.getDiveSheets(session['id'])
+	#sheets = db.getDiveSheets(session['id'])
 	return render_template('divesheets.html',sheets=sheets)
 
 @app.route('/divesheets/<int:id>')
@@ -16,9 +16,9 @@ def sheet(id):
 	if 'id' not in session:
 		return redirect(url_for('index'))
 	sheet = [[1,'My Super Awesome Dive Sheet',''],]
-	sheet = db.getDiveSheet(id)
+	#sheet = db.getDiveSheet(id)
 	dives = [['A204', '1', 'Forward Dive', 'Tuck', '1.4', '10'],['A204', '1', 'Hurricane Dive', 'Tuck', '1.4', '10']]
-	dives=db.getDivesInSheet(id)
+	#dives=db.getDivesInSheet(id)
 	sheet = sheet[0]
 	return render_template('divesheet.html', sheet=sheet, dives=dives)
 
@@ -37,7 +37,7 @@ def createDiveSheet():
 	# Create default sheet to be created
 	sheet = [1,'My Super Awesome Dive Sheet','']
 	doableDives = [[1,'Forward Dive','A',1],[2, 'Backward Dive','B',3],[3,'Reverse Hurricane','B',1]]
-	doableDives = db.getDoableDives(session['id'])
+	#doableDives = db.getDoableDives(session['id'])
 	dives = []
 	if request.method == 'POST':
 		if request.form['title']:
@@ -46,13 +46,13 @@ def createDiveSheet():
 			message = "Title cannot be empty"
 		for i in range(1,11):
 			if request.form['dive'+str(i)]:
-				dives.append(request.form['dive'+str(i)]) # This is an ID
+				dives.append(int(request.form['dive'+str(i)])) # This is an ID
 			else:
 				message = "Dive number " + str(i) + " cannot be empty!"
 		if not message:
 			id = 1
 			# Commit the sheet to the database, get the id back as id
-			id = db.createDiveSheet(sheet, dives, session['id'])
+			#id = db.createDiveSheet(sheet, dives, session['id'])
 			return redirect(url_for('sheet', id=id))
 	return render_template('createdivesheet.html',message=message, sheet=sheet, doableDives=doableDives)
 	
