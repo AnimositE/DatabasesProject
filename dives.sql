dc731d5b1a483c465b928eec7e01f2e7de0bbd7c
Faye Li Si Fi <lis@rose-hulman.edu>
2014-02-17 16:48:50 -0500

    Input LOTS OF DIVES!

--- dives.sql
+++ dives.sql
@@ -32,7 +32,104 @@ INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forwa
 INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Forward 2 1/2 Somersaults','105',3,'Forward','C',2.2);
 
 -- All of these are entered
+--Back Group
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Dive','201',1,'Back','A',1.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Dive','201',1,'Back','B',1.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Dive','201',1,'Back','C',1.5);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Dive','201',3,'Back','A',1.9);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Dive','201',3,'Back','B',1.8);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Dive','201',3,'Back','C',1.7);
 
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Somersault','202',1,'Back','A',1.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Somersault','202',1,'Back','B',1.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Somersault','202',1,'Back','C',1.5);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Somersault','202',3,'Back','A',1.8);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Somersault','202',3,'Back','B',1.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back Somersault','202',3,'Back','C',1.6);
 
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 1 1/2 Somersault','203',1,'Back','A',2.5);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 1 1/2 Somersault','203',1,'Back','B',2.3);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 1 1/2 Somersault','203',1,'Back','C',2.0);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 1 1/2 Somersault','203',3,'Back','A',2.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 1 1/2 Somersault','203',3,'Back','B',2.2);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 1 1/2 Somersault','203',3,'Back','C',1.9);
 
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 2 Somersaults','204',1,'Back','B',2.5);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 2 Somersaults','204',1,'Back','C',2.2);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 2 Somersaults','204',3,'Back','A',2.5);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 2 Somersaults','204',3,'Back','B',2.3);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 2 Somersaults','204',3,'Back','C',2.0);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 2 1/2 Somersaults','205',1,'Back','B',3.2);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 2 1/2 Somersaults','205',1,'Back','C',3.0);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 2 1/2 Somersaults','205',3,'Back','B',3.0);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Back 2 1/2 Somersaults','205',3,'Back','C',2.8);
+--Back Group
+
+--Reverse Group
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Dive','301',1,'Reverse','A',1.8);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Dive','301',1,'Reverse','B',1.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Dive','301',1,'Reverse','C',1.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Dive','301',3,'Reverse','A',2.0);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Dive','301',3,'Reverse','B',1.9);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Dive','301',3,'Reverse','C',1.8);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Somersault','302',1,'Reverse','A',1.8);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Somersault','302',1,'Reverse','B',1.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Somersault','302',1,'Reverse','C',1.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Somersault','302',3,'Reverse','A',1.9);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Somersault','302',3,'Reverse','B',1.8);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse Somersault','302',3,'Reverse','C',1.7);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 1 1/2 Somersault','303',1,'Reverse','A',2.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 1 1/2 Somersault','303',1,'Reverse','B',2.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 1 1/2 Somersault','303',1,'Reverse','C',2.1);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 1 1/2 Somersault','303',3,'Reverse','A',2.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 1 1/2 Somersault','303',3,'Reverse','B',2.3);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 1 1/2 Somersault','303',3,'Reverse','C',2.0);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 Somersaults','304',1,'Reverse','A',2.9);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 Somersaults','304',1,'Reverse','B',2.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 Somersaults','304',1,'Reverse','C',2.3);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 Somersaults','304',3,'Reverse','A',2.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 Somersaults','304',3,'Reverse','B',2.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 Somersaults','304',3,'Reverse','C',2.1);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 1/2 Somersaults','305',1,'Reverse','B',3.2);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 1/2 Somersaults','305',1,'Reverse','C',3.0);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 1/2 Somersaults','305',3,'Reverse','A',3.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 1/2 Somersaults','305',3,'Reverse','B',3.0);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Reverse 2 1/2 Somersaults','305',3,'Reverse','C',2.8);
+--Reverse Group
+
+--Inward Group
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Dive','401',1,'Inward','A',1.8);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Dive','401',1,'Inward','B',1.5);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Dive','401',1,'Inward','C',1.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Dive','401',3,'Inward','A',1.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Dive','401',3,'Inward','B',1.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Dive','401',3,'Inward','C',1.3);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Somersault','402',1,'Inward','A',2.0);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Somersault','402',1,'Inward','B',1.7);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Somersault','402',1,'Inward','C',1.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Somersault','402',3,'Inward','A',1.8);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Somersault','402',3,'Inward','B',1.5);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward Somersault','402',3,'Inward','C',1.4);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 1 1/2 Somersault','403',1,'Inward','B',2.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 1 1/2 Somersault','403',1,'Inward','C',2.2);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 1 1/2 Somersault','403',3,'Inward','B',2.1);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 1 1/2 Somersault','403',3,'Inward','C',1.9);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 2 Somersaults','404',1,'Inward','B',3.0);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 2 Somersaults','404',1,'Inward','C',2.8);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 2 Somersaults','404',3,'Inward','B',2.6);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 2 Somersaults','404',3,'Inward','C',2.4);
+
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 2 1/2 Somersaults','405',1,'Inward','B',3.4);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 2 1/2 Somersaults','405',1,'Inward','C',3.1);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 2 1/2 Somersaults','405',3,'Inward','B',3.0);
+INSERT INTO Dives (name, number, height, diveGroup, position, dd) VALUES ('Inward 2 1/2 Somersaults','405',3,'Inward','C',2.7);
+--Reverse Group
 
